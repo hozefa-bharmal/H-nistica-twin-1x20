4b8f20d8b3444bc27787738c329c8e08c2e7e18c
Prasanna <annasarps@gmail.com>
2020-04-23 16:37:45 +0530

    [TASK] Pseudo code to get maximum frequency bound value
    Add pseudo code to get the maximum frequency bound value from
    WSS module. Provide details about the function and it's use to
    define waveplan. Add packet structure formation in function description
    where the Message ID, Object ID etc are given.
    
    Add logic in code to transmit and receive the packet to/from UART.
    Explain logic to extract specific bytes inorder to get the desired data.

--- nistica-wss/nistica_wss_library.c
+++ nistica-wss/nistica_wss_library.c
@@ -188,3 +188,79 @@ int get_minimum_frequency_bound_of_nistica_wss_module( unsigned int uart_port_nu
 	If any validation fail -> 
 				Return error with appropriate error message/reason for failure
 }
+
+/**
+ * @brief Function will Get Maximum Frequency Bound value of Nistica WSS module
+ *
+ * This function reads the upper possible channel edge value from WSS module.
+ * The value remains the lower boundary to define the waveplan allowed for the flexible
+ * waveplan management commands.
+ * Function forms a command structure consisting of Message ID, Command, Object ID,
+ * Instance, Parameter and Checksum.
+ * The Transmit and Receive packet is encoded with '0xdd 0x01' in the beginning and
+ * '0xdd 0x02' in the end representing RS-232 frame marker required to ensure
+ * synchronization between control module and WSS.
+ *
+ * Tx packet to WSS module : 0xdd 0x01 MID LEN CMD OBJ INS PAR SUM 0xdd 0x02
+ *		MID=0x19; OBJ=0x80; INS=0x05; PAR=0x00; SUM (1 byte) = XOR from MID to PAR
+ *
+ * Response from WSS : 0xdd 0x01 MID LEN RES DATA SUM 0xdd 0x02
+ *		DATA (2 bytes)
+ *
+ * Function takes two arguments - UART port number and character pointer. UART port
+ * is used to transmit and receive data packets from/to Nistica WSS module. Certain
+ * validation in the receive packet such as Message ID, Length and Result is done
+ * by the function and the Vendor name data is extracted and returned.
+ * 
+ * @param uart_port_number
+ * an unsigned integer, represents the particular UART port to which Nistica WSS is connected
+ *
+ *	Example: 1
+ *
+ * @param vendor_name
+ * a character pointer, represents the data in which Vendor name is returned.
+ *
+ *	Example: Nistica
+ *
+ * @return an integer
+ *  <BR>  0  : Success
+ *  <BR> -1  : Failure
+**/
+int get_maximum_frequency_bound_of_nistica_wss_module( unsigned int uart_port_number, unsigned short* maximum_frequency_bound )
+{
+	char packet_to_transmit[] = { 0xdd, 0x01, 0x19, 0x05, READ_CMD, 0x80, 0x05, 0x00, 0x9B, 0xdd, 0x02 };
+	char uart_received_packet_return[255]={0};
+
+	int transmit_packet=0,
+		receive_packet=0;
+
+	unsigned int length_of_packet_to_transmit=0;
+	unsigned int length_of_received_packet_return=0;
+
+	length_of_packet_to_transmit = strlen(packet_to_transmit); //11
+
+	transmit_packet = transmit_packet_via_uart_port(uart_port_number, packet_to_transmit, length_of_packet_to_transmit);
+	if(SUCCESS != transmit_packet)
+	{
+		printf("Error : Failed to transmit packet via UART Port in get_maximum_frequency_bound_of_nistica_wss_module()\n");
+		return FAILURE;
+	}
+
+	//usleep(WAIT_TIME_TO_RECEIVE_PACKET_FROM_MODULE);
+
+	receive_packet = receive_packet_via_uart_port(uart_port_number, uart_received_packet_return, &length_of_received_packet_return);
+	if(SUCCESS != receive_packet)
+	{
+   		printf("Error : Failed to receive packet via UART Port in get_maximum_frequency_bound_of_nistica_wss_module()\n");
+    	return FAILURE;
+	}
+
+	Validate MID -> packet_to_transmit[2] == uart_received_packet_return[2];
+	Validate RES -> SUCCESS == uart_received_packet_return[4];
+	
+	If above validation passes ->
+				extract Maximum Frequency bound value from uart_received_packet_return[5] and uart_received_packet_return[6], OR it and store the result in maximum_frequency_bound
+	
+	If any validation fail -> 
+				Return error with appropriate error message/reason for failure
+}
--- nistica-wss/nistica_wss_library.h
+++ nistica-wss/nistica_wss_library.h
@@ -11,3 +11,5 @@
 int get_vendor_name_of_nistica_wss_module( unsigned int uart_port_number, unsigned char *vendor_name );
 
 int get_minimum_frequency_bound_of_nistica_wss_module( unsigned int uart_port_number, unsigned short* minimum_frequency_bound );
+
+int get_maximum_frequency_bound_of_nistica_wss_module( unsigned int uart_port_number, unsigned short* maximum_frequency_bound );
