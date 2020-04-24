b225df47f1abbc8d4d26ecb294441169b1a48f6d
KPremsai <kamavarampremsai@gmail.com>
2020-04-24 19:06:40 +0530

    [TASK]:
    Add the pseudo code for get function Table and Rows feature
    
    [Description]:
    
    Add the pseudo code for the function get_module_supports_table_and_row_commands_of_nistica_wss_module
    in order to implement this property for the WSS Module.

--- nistica-wss/nistica_wss_library.c
+++ nistica-wss/nistica_wss_library.c
@@ -629,3 +629,76 @@ int get_module_supports_multi_commands_of_nistica_wss_module( unsigned int uart_
 	If any validation fail ->
 				Return error with appropriate error message/reason for failure
 }
+
+/**
+ * @brief Function will Get Table and Rows Fearture value present Nistica WSS module
+ *
+ * This function reads the table and rows fearture value that is defined in WSS module.
+ * Function forms a command structure consisting of Message ID, Command, Object ID,
+ * Instance, Parameter and Checksum.
+ * The Transmit and Receive packet is encoded with '0xdd 0x01' in the beginning and
+ * '0xdd 0x02' in the end representing RS-232 frame marker required to ensure
+ * synchronization between control module and WSS.
+ *
+ * Tx packet to WSS module : 0xdd 0x01 MID LEN CMD OBJ INS PAR SUM 0xdd 0x02
+ *		MID=0x10; OBJ=0x80; INS=0x0E; PAR=0x00; SUM (1 byte) = XOR from MID to PAR
+ *
+ * Response from WSS : 0xdd 0x01 MID LEN RES DATA SUM 0xdd 0x02
+ *		DATA (2 bytes)
+ *
+ * Function takes two arguments - UART port number and character pointer. UART port
+ * is used to transmit and receive data packets from/to Nistica WSS module. Certain
+ * validation in the receive packet such as Message ID and Result is done by the function.
+ * Required Data is extracted and returned.
+ *
+ * @param uart_port_number
+ * an unsigned integer, represents the particular UART port to which Nistica WSS is connected
+ *
+ *	Example: 1
+ *
+ * @param table_row_cmd
+ * a signed short pointer, represents the data in which table and rows fearture value is returned.
+ * This variable will have '1' if the module supports long commands feature, '0' if not.
+ *
+ * @return an integer
+ *  <BR>  0 : Success
+ *  <BR> -1  : Failure
+**/
+int get_module_supports_table_and_row_commands_of_nistica_wss_module( unsigned int uart_port_number,short* table_row_cmd )
+{
+	char packet_to_transmit[] = { 0xdd, 0x01, 0x11, 0x05, READ_CMD, 0x80, 0x0E, 0x00, 0x98, 0xdd, 0x02 };
+	char uart_received_packet_return[255]={0};
+
+	int transmit_packet=0,
+	    receive_packet=0;
+
+	unsigned int length_of_packet_to_transmit=0;
+	unsigned int length_of_received_packet_return=0;
+
+	length_of_packet_to_transmit = strlen(packet_to_transmit);
+
+	transmit_packet = transmit_packet_via_uart_port(uart_port_number, packet_to_transmit, length_of_packet_to_transmit);
+	if(SUCCESS != transmit_packet)
+	{
+        printf("Error : Failed to transmit packet via UART Port in get_module_supports_multi_commands_of_nistica_wss_module()\n");
+        return FAILURE;
+	}
+
+	//usleep(WAIT_TIME_TO_RECEIVE_PACKET_FROM_MODULE);
+
+	receive_packet = receive_packet_via_uart_port(uart_port_number, uart_received_packet_return, &length_of_received_packet_return);
+	if(SUCCESS != receive_packet)
+	{
+        printf("Error : Failed to receive packet via UART Port in get_module_supports_multi_commands_of_nistica_wss_module()\n");
+        return FAILURE;
+	}
+
+    Validate MID -> packet_to_transmit[2] == uart_received_packet_return[2];
+    Validate RES -> SUCCESS == uart_received_packet_return[4];
+
+    If above validation passes ->
+				extract table_row_cmd value from uart_received_packet_return[5] and uart_received_packet_return[6], OR it and store the result in maximum_ocm_id_value
+
+	If any validation fail ->
+				Return error with appropriate error message/reason for failure
+}
