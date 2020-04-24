7bbed9f334ae5d488f6ac88f2ec5c13c9b5235e9
Gattu shivakrishna <shiva.krishna17892@gmail.com>
2020-04-24 20:51:59 +0530

    [Task] : Add pseudo code for set_warm_boot_mode_for_nistica_wss_module function
    Add pseudo code for set_warm_boot_mode_for_nistica_wss_module function and
    Added function declaration in header file.
    
    Task Reference #0000
    
    
    Signed-off-by: Gattu shivakrishna <shiva.krishna17892@gmail.com>

--- nistica-wss/nistica_wss_library.h
+++ nistica-wss/nistica_wss_library.h
@@ -30,3 +30,5 @@ int get_module_status_of_nistica_wss_module( unsigned int uart_port_number, shor
 int get_boot_mode_of_nistica_wss_module( unsigned int uart_port_number, short* boot_mode );
 
 int set_cold_boot_mode_for_nistica_wss_module( unsigned int uart_port_number, short* boot_mode );
+
+int set_warm_boot_mode_for_nistica_wss_module( unsigned int uart_port_number, short* boot_mode );
