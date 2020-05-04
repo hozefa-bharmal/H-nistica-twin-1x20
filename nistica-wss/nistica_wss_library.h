512ecf8510ace916181cc1caef2477191582d2a8
shanmukha1112 <shanmukhanch@gmail.com>
2020-05-04 07:54:48 +0530

    [TASK] Add pseudo code to get partition 2 firmware version
    Provide details about the function and it's use. Add packet
    structure formation in function description where the Message
    ID, Object ID etc are given.
    
    Add logic in code to transmit and receive the packet to/from UART.
    Explain logic to extract specific bytes inorder to get the desired data.

--- nistica-wss/nistica_wss_library.h
+++ nistica-wss/nistica_wss_library.h
@@ -52,6 +52,8 @@ int get_booted_partition_number_of_nistica_wss_module( unsigned int uart_port_nu
 
 int get_partition_1_firmware_version_of_nistica_wss_module( unsigned int uart_port_number, unsigned char *firmware_version );
 
+int get_partition_2_firmware_version_of_nistica_wss_module( unsigned int uart_port_number, unsigned char *firmware_version );
+
  /************************************************
 *************************************************
 *		Channel Configurations
